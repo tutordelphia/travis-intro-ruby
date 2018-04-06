0c99d9d9cceac97e4f0ab0206307252cc3d76d70
Hiro Asari <asari.ruby@gmail.com>
2018-04-06 09:55:28 -0400

    Sleep a little before running test
    So WEBrick can accept connections

--- test/test_server.rb
+++ test/test_server.rb
@@ -6,6 +6,7 @@ require_relative '../lib/server'
 class TestMyServer < Test::Unit::TestCase
   def test_server
     thr = Thread.new { MyServer.start }
+    sleep 2
     assert TCPSocket.new('localhost', 8000)
   end
 end
